3537795610c284b743879b74b1a1b207b8df1ace
3doriTea <sorasora3program@gmail.com>
2025-10-25 14:44:28 +0900

    非同期でループするようにした

--- Program.cs
+++ Program.cs
@@ -10,29 +10,54 @@ namespace PomoServer
 		{
 			var listener = new TcpListener(IPAddress.Parse("192.168.3.159"), 80);
 			listener.Start();
-			Console.WriteLine($"Listening on {listener.LocalEndpoint}");
-	
-			using var client = listener.AcceptTcpClient();
-			using var stream = client.GetStream();
-	
-			var buffer = new byte[1024];
-			int length = stream.Read(buffer, 0, buffer.Length);
-			var request = Encoding.UTF8.GetString(buffer);
-			Console.WriteLine(request);
-	
-			if (request.StartsWith("GET"))
+
+			Console.CancelKeyPress += (sender, e) =>
 			{
-				var response = string.Join("\r\n",
-				[
-					"HTTP/1.1 200 OK",
-					"Content-Type: text/plain",
-					"Content-Length: 11",
-					"",
-					"Hello World"
-				]);
-	
-				byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-				stream.Write(responseBytes, 0, responseBytes.Length);
+				listener.Stop();
+			};
+
+			Console.WriteLine($"Listening on {listener.LocalEndpoint}");
+
+			while (true)
+			{
+				try
+				{
+					var client = listener.AcceptTcpClient();
+
+					Task.Run(() =>
+					{
+						using var stream = client.GetStream();
+
+						var buffer = new byte[1024];
+						int length = stream.Read(buffer, 0, buffer.Length);
+						var request = Encoding.UTF8.GetString(buffer);
+						Console.WriteLine("request done");
+						//Console.WriteLine(request);
+
+						if (request.StartsWith("GET"))
+						{
+							var response = string.Join("\r\n",
+							[
+								"HTTP/1.1 200 OK",
+								"Content-Type: text/plain",
+								"Content-Length: 11",
+								"",
+								"Hello World"
+							]);
+
+							byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+							stream.Write(responseBytes, 0, responseBytes.Length);
+						}
+
+						client.Close();
+						client.Dispose();
+					});
+				}
+				catch (SocketException e)
+				{
+					Console.WriteLine(e);
+					break;
+				}
 			}
 		}
 	}
